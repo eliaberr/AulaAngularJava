14d60a2d151c80f4eb8329bb13d53bcc072c7acf
Eliabr Rodrigues <eliabe859@gmail.com>
2023-11-17 17:45:45 -0300

    first commit

---
+++ src/main/java/br/com/fatec/hellospringboot/entities/Client.java
@@ -0,0 +1,46 @@
+package br.com.fatec.hellospringboot.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_CLIENT")
+
+public class Client {
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String name;
+    private double balance;
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    
+}
---
+++ src/main/java/br/com/fatec/hellospringboot/resources/ClientController.java
@@ -0,0 +1,44 @@
+package br.com.fatec.hellospringboot.resources;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import br.com.fatec.hellospringboot.entities.Client;
+import br.com.fatec.hellospringboot.service.ClientsService;
+
+@RestController
+public class ClientController {
+    
+    @Autowired
+    private ClientsService service;
+
+    
+    @GetMapping("clients")
+    public ResponseEntity<List<Client>> getClients(){
+       return ResponseEntity.ok(service.getClients());
+    }
+
+    @GetMapping("clients/{id}")
+    public ResponseEntity<Client> getClientById(@PathVariable int id){
+    return ResponseEntity.ok(service.getClientById(id));
+    }
+
+    @DeleteMapping("clients/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable int id){
+        return ResponseEntity.noContent().build();
+    }
+
+
+  
+
+
+}
